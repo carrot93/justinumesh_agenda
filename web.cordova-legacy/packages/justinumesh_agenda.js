9313a28ff0d6eb13e288657136c73fad52472294
carrot93 <carrot.live@hotmail.com>
2019-06-16 19:28:01 +0800

    first commit:recover the workpoint of author

---
+++ web.cordova-legacy/packages/justinumesh_agenda.js
@@ -0,0 +1,20 @@
+(function(){
+
+/////////////////////////////////////////////////////////////////////////////
+//                                                                         //
+// packages/justinumesh_agenda/agenda.js                                   //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
+                                                                           //
+// Write your package code here!                                           // 1
+                                                                           // 2
+// Variables exported by this module can be imported by other packages and
+// applications. See agenda-tests.js for an example of importing.          // 4
+                                                                           // 5
+                                                                           // 6
+Agenda = function() {                                                      // 7
+   return Npm.require('agenda');                                           // 8
+}                                                                          // 9
+/////////////////////////////////////////////////////////////////////////////
+
+}).call(this);
---
+++ web.cordova/agenda.js
@@ -0,0 +1,9 @@
+// Write your package code here!
+
+// Variables exported by this module can be imported by other packages and
+// applications. See agenda-tests.js for an example of importing.
+
+
+Agenda = function() {
+   return Npm.require('agenda');
+}
